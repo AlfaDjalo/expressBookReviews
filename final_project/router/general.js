c104bce1498ccac0e486b089defbf5d9a226f52b
AlfaDjalo <david_a_collier@hotmail.com>
2024-07-19 09:54:58 -0400

    Completed project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,19 +42,58 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(books,null,4));
 });
 
+// Get the book list available in the shop using promises
+public_users.get('/async-get-books',function (req, res) {
+
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+
+  });
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     // Retrieve the email parameter from the request URL and send the corresponding friend's details
     const isbn = req.params.isbn;
     return res.send(books[isbn]); });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    // Extract the author parameter from the request URL
-    const author = req.params.author;
-    const result = [];
+// Get the book list based on ISBN using promises
+public_users.get('/async-get-books/isbn/:isbn',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        resolve(res.send(books[isbn]));
+      });
 
-    for (let key in books) {
+    get_books.then(() => console.log("Promise for Task 11 resolved"));
+    });
+    
+// Get book details based on author using promises
+public_users.get('/async-get-books/author/:author',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        // Extract the author parameter from the request URL
+        const author = req.params.author;
+        const result = [];        
+          
+        for (let key in books) {
+            if (books[key].author === author) {
+                result.push(books[key]);
+            }
+        }
+        resolve(res.status(200).json(result));
+        });
+        
+        get_books.then(() => console.log("Promise for Task 12 resolved"));
+    });
+    
+    // Get book details based on author
+    public_users.get('/author/:author',function (req, res) {
+        // Extract the author parameter from the request URL
+        const author = req.params.author;
+        const result = [];
+        
+        for (let key in books) {
         if (books[key].author === author) {
             result.push(books[key]);
         }
@@ -77,6 +116,24 @@ public_users.get('/title/:title',function (req, res) {
     // return res.send(result);
     return res.status(200).json(result);
 });
+
+// Get book details based on title using promises
+public_users.get('/async-get-books/title/:title',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        // Extract the author parameter from the request URL
+        const title = req.params.title;
+        const result = [];        
+          
+        for (let key in books) {
+            if (books[key].title === title) {
+                result.push(books[key]);
+            }
+        }
+        resolve(res.status(200).json(result));
+        });
+        
+        get_books.then(() => console.log("Promise for Task 13 resolved"));
+    });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
